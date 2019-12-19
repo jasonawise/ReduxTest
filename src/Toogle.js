107c6be2e8b4b1feea7c8c3ae29947501c14b906
Jason Wise <jasonwise@Matthews-MacBook-Pro.local>
2019-12-19 16:02:14 -0500

    creating actions

--- src/Toogle.js
+++ src/Toogle.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import toogleMessage from "./actions";
 const Toogle = ({ messageVisibility, dispatch }) => (
   <div>
     {messageVisibility && <p>You will see this if toggled</p>}
-    <button onClick={() => dispatch({ type: "TOOGLE_MESSAGE" })}>
-      Toogle Me
-    </button>
+    <button onClick={() => dispatch(toogleMessage())}>Toogle Me</button>
   </div>
 );
 
